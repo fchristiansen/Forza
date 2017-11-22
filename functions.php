9388aacd9cd256d23b94c20a5b29830eaad110da
fchristiansen <felipe@seo2.cl>
2017-11-22 14:51:43 -0300

    ajustes servicios

--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
-<?php 
+<?php
   function iniciarTema(){
-    // ACTIVA IMAGENES DESTACADAS  
+    // ACTIVA IMAGENES DESTACADAS
      add_theme_support( 'post-thumbnails' );
-     add_image_size('logo_cliente', 200, 200, true); 
-     add_image_size('img_equipo', 400, 400, true); 
-     add_image_size('portafolio_video', 700, 400, true); 
-     add_image_size('portafolio_galeria', 400, 400, true); 
-     // add_image_size('img-noticia',730 ,410 , true); 
-     // add_image_size('home_cortos',730 ,410 , true); 
-    
-    // Activar Titulo 
+     add_image_size('logo_cliente', 200, 200, true);
+     add_image_size('img_equipo', 400, 400, true);
+     add_image_size('portafolio_video', 700, 400, true);
+     add_image_size('portafolio_galeria', 400, 400, true);
+     // add_image_size('img-noticia',730 ,410 , true);
+     // add_image_size('home_cortos',730 ,410 , true);
+
+    // Activar Titulo
     add_theme_support( 'title-tag' );
 
-    // Activa Menu Dinamico  
+    // Activa Menu Dinamico
       register_nav_menus( array(
           'primary' => __( 'Menu', 'menu_principal' ),
       ));
   }
- 
+
     // Necesario para menu Bootstrap
     require_once('wp_bootstrap_navwalker.php');
     // Cuando ocurra 'after_setup_theme, invocar "iniciarTema"
@@ -25,7 +25,7 @@
 
     // remueve adminbar en front
      add_filter( 'show_admin_bar', '__return_false' );
-     
+
 
 function custom_excerpt_length( $length ) {
   return 30;
@@ -61,7 +61,7 @@ add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 // }
 
 
-// carga css 
+// carga css
 function theme_styles() {
   wp_enqueue_style('loader', get_template_directory_uri() . '/assets/css/dataurl.css');
   wp_enqueue_style('boostrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
@@ -79,7 +79,7 @@ function theme_styles() {
 function jquery_cdn() {
    if (!is_admin()) {
         wp_deregister_script('jquery');
-        wp_register_script('jquery', 'http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js', false , null ); 
+        wp_register_script('jquery', 'http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js', false , null );
         wp_enqueue_script('jquery');
    }
 }
@@ -89,7 +89,7 @@ function theme_js(){
   wp_enqueue_script( 'pace', get_template_directory_uri() . '/assets/js/pace.min.js', array('jquery'), '1.0', false );
   wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/assets/js/bootstrap.min.js', array('jquery'),'3.3.7', true);
   wp_enqueue_script( 'formValidation', get_template_directory_uri() . '/assets/js/formValidation.min.js', array('jquery'), '3.0', true );
-  
+
   wp_enqueue_script( 'formValidationLangEs', get_template_directory_uri() . '/assets/js/language/es_ES.js', array('jquery'), '3.0', true );
   wp_enqueue_script( 'formValidationBootstrap', get_template_directory_uri() . '/assets/js/framework/bootstrap.min.js', array('jquery'), '3.0', true );
   wp_enqueue_script('wow', get_template_directory_uri() . '/assets/js/wow.min.js', array('jquery'),'1.1.3', true);
@@ -108,7 +108,7 @@ function muestra_galeria($post_id = false, $exclude = true, $cantidad = -1) {
 		if (!$post_id){
 			$post_id = $post->ID;
 		}
-		$args = array( 'post_type' => 'attachment', 'numberposts' => $cantidad, 'post_status' => null, 'post_parent' => $post_id, 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC'); 
+		$args = array( 'post_type' => 'attachment', 'numberposts' => $cantidad, 'post_status' => null, 'post_parent' => $post_id, 'post_mime_type' => 'image', 'orderby' => 'menu_order', 'order' => 'ASC');
 		if ($exclude){
 			$args = array_merge( $args, array( 'post__not_in' => array( get_post_thumbnail_id(), $imagen_chica ) ) );
 		}
--- include-modales-servicios.php
+++ include-modales-servicios.php
@@ -1,5 +1,5 @@
-        
-      <?php 
+
+      <?php
       $args = array (
           'post_type' => 'servicios',
            'p' => 128
@@ -23,7 +23,7 @@
                     </div>
                     <div class="col-md-5">
                       <img src="<?php echo get('iconos_icono_modal'); ?>" alt="<?php the_title();?>" class="img-responsive center-block servicio_md">
-                    </div> 
+                    </div>
                   </div>
                 </div>
                 <div class="modal-footer"> </div>
@@ -32,9 +32,9 @@
           </div> <!-- modal servicio 1 -->
 
         <?php endwhile; ?>
-        <?php wp_reset_query(); ?> 
+        <?php wp_reset_query(); ?>
 
-        <?php 
+        <?php
         $args = array (
             'post_type' => 'servicios',
              'p' => 129
@@ -59,7 +59,7 @@
                           <img src="<?php echo get('iconos_icono_modal'); ?>" alt="<?php the_title();?>" class="img-responsive center-block servicio_md">
                     </div>
 
-                  </div>               
+                  </div>
                   </div>
                 <div class="modal-footer">
                 </div>
@@ -67,8 +67,8 @@
             </div>
           </div> <!-- modal servicio 2 -->
         <?php endwhile; ?>
-        <?php wp_reset_query(); ?> 
-            <?php 
+        <?php wp_reset_query(); ?>
+            <?php
             $args = array (
                 'post_type' => 'servicios',
                  'p' => 130
@@ -100,5 +100,42 @@
             </div>
           </div> <!-- modal servicio 3 -->
            <?php endwhile; ?>
-          <?php wp_reset_query(); ?> 
+          <?php wp_reset_query(); ?>
+
+          <?php
+            $args = array (
+                'post_type' => 'servicios',
+                 'p' => 239
+              );
+              $the_query = new WP_Query ($args);
+          ?>
+          <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+          <div class="modal fade custom_modal" id="servicio4" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col-md-7">
+                     <img src="<?php echo get('iconos_icono_modal'); ?>" alt="<?php the_title();?>" class="img-responsive center-block servicio_xs">
+                      <?php the_title( '<h4>', '</h4>'); ?>
+                      <?php the_content() ?>
+                    </div>
+                    <div class="col-md-5">
+                      <img src="<?php echo get('iconos_icono_modal'); ?>" alt="<?php the_title();?>" class="img-responsive center-block servicio_md">
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                </div>
+              </div> <!-- modal content -->
+            </div>
+          </div> <!-- modal servicio 4 -->
+           <?php endwhile; ?>
+          <?php wp_reset_query(); ?>
+
+
+
 
